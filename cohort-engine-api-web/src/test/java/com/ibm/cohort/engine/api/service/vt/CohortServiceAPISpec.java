c12bcd57ee090a5461ebe645d8cb8767ead6b6a3
matthew.sargent <matthew.sargent@ibm.com>
2021-02-05 15:31:59 -0500

    checkpoint

--- cohort-engine-api-web/src/test/java/com/ibm/cohort/engine/api/service/vt/CohortServiceAPISpec.java
+++ cohort-engine-api-web/src/test/java/com/ibm/cohort/engine/api/service/vt/CohortServiceAPISpec.java
@@ -9,5 +9,6 @@ package com.ibm.cohort.engine.api.service.vt;
 public class CohortServiceAPISpec {
 	public static final String CREATE_DELETE_EVALUATION_PATH = "/{version}/evaluation";
 	public static final String GET_EVALUATION_STATUS_PATH = "/{version}/evaluation/status";
+	public static final String TRANSLATION_PATH = "/translator";
 	
 }
---
+++ cohort-engine-api-web/src/test/java/org/mitre/bonnie/cqlTranslationServer/TranslationResourceTest.java
@@ -0,0 +1,14 @@
+package org.mitre.bonnie.cqlTranslationServer;
+
+
+import java.net.URISyntaxException;
+
+import org.junit.Test;
+
+public class TranslationResourceTest {
+	
+	@Test
+	public void testValidCql() throws URISyntaxException {
+
+	}
+}
