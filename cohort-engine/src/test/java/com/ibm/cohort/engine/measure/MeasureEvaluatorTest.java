38e5e986c6d71662d9e20a2f8492f2913d819c8d
Jill Doty <jdoty@us.ibm.com>
2021-02-17 11:26:49 -0500

    In flight work

--- cohort-engine/src/test/java/com/ibm/cohort/engine/measure/MeasureEvaluatorTest.java
+++ cohort-engine/src/test/java/com/ibm/cohort/engine/measure/MeasureEvaluatorTest.java
@@ -349,10 +349,12 @@ public class MeasureEvaluatorTest extends BaseMeasureTest {
 		Patient patient = getPatient("123", AdministrativeGender.MALE, "1970-10-10");
 		mockFhirResourceRetrieval(patient);
 
-		Library library = mockLibraryRetrieval("TestAdultMales", "cql/fhir-measure/test-adult-males.cql");
-		Library library2 = mockLibraryRetrieval("TestAdultMales2", "cql/fhir-measure/test-adult-males2.cql");
-		Library library3 = mockLibraryRetrieval("TestAdultMales3", "cql/fhir-measure/test-adult-males3.cql");
-
+		Library library2 = mockLibraryRetrieval("TestAdultMales2", DEFAULT_VERSION, "cql/fhir-measure/test-adult-males2.cql");
+		Library library3 = mockLibraryRetrieval("TestAdultMales3", DEFAULT_VERSION, "cql/fhir-measure/test-adult-males3.cql");
+		Library library = mockLibraryRetrieval("TestAdultMales", DEFAULT_VERSION, "cql/fhir-measure/test-adult-males.cql");
+		library.addRelatedArtifact(asRelation(library2));
+		library.addRelatedArtifact(asRelation(library3));
+		
 		expressionsByPopulationType.clear();
 		expressionsByPopulationType.put(MeasurePopulationType.INITIALPOPULATION, INITIAL_POPULATION);
 		expressionsByPopulationType.put(MeasurePopulationType.DENOMINATOR, DENOMINATOR);
@@ -396,7 +398,7 @@ public class MeasureEvaluatorTest extends BaseMeasureTest {
 		Patient patient = getPatient("123", AdministrativeGender.MALE, "1970-10-10");
 		mockFhirResourceRetrieval(patient);
 
-		Library library = mockLibraryRetrieval("TestAdultMales", "cql/fhir-measure/test-adult-males.cql");
+		Library library = mockLibraryRetrieval("TestAdultMales", DEFAULT_VERSION, "cql/fhir-measure/test-adult-males.cql");
 
 		expressionsByPopulationType.clear();
 		expressionsByPopulationType.put(MeasurePopulationType.INITIALPOPULATION, INITIAL_POPULATION);
